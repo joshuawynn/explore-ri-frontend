08a0054f467d73e70182e662f563ae2266302f78
joshuawynn <joshua.a.wynn@gmail.com>
2024-02-15 13:34:52 -0500

    Heroku update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = 'http://localhost:3001/'
+export const BASE_URL = "https://explore-ri-backend-b8b2d1c6dd79.herokuapp.com/"
 
 const Client = axios.create({baseURL: BASE_URL})
 
